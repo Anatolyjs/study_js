01be94c392ec47491435955a9676435023df8aeb
Anatoly Kebets <akebecz@imbox.ru>
2021-04-05 12:23:56 +0300

    Done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 let money = 2000, 
 income = 'freelance', 
 addExpenses = 'Internet, taxi, communal flat',
@@ -11,3 +12,31 @@ console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' долларов');
 console.log(addExpenses.toLowerCase().split(', '));
 console.log(budgetDay);
+money = prompt('Ваш месячный доход?');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+console.log(addExpenses);
+deposit = confirm('Есть ли у вас депозит в банке?');
+const expenses1 = prompt('Введите обязательную статью расходов?');
+const expenses2 = prompt('Введите обязательную статью расходов?');
+const amount1 = +prompt('Во сколько это обойдется?');
+const amount2 = +prompt('Во сколько это обойдется?');
+const budgetMonth = money - (amount1 + amount2);
+console.log(budgetMonth);
+console.log('Цель будет достигнута за ' + Math.ceil(mission / budgetMonth) + ' месяцев');
+console.log( Math.round(budgetDay = budgetMonth / 30) );
+if (budgetDay  >= 1200) {
+   console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+   console.log('У вас средний уровень дохода');
+} else if (budgetDay > 0 && budgetDay < 600) {
+   console.log('К сожалению, у вас уровень дохода ниже среднего');
+} else {
+   console.log('Что-то пошло не так');
+}
+
+
+
+
+
+
+
