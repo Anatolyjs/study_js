8bce2f66c4fab2096fb5e0eb2eec1258d8174455
Anatoly Kebets <akebecz@imbox.ru>
2021-04-06 21:56:19 +0300

    finnaly

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
 'use strict';
 const money = +prompt('Ваш месячный доход?'), 
-income = 'freelance', 
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую').toLowerCase().split(', '),
-deposit = confirm('Есть ли у вас депозит в банке?'), 
-mission = 20000, 
-period = 12, 
-expenses1 = prompt('Введите обязательную статью расходов?'),
-amount1 = +prompt('Во сколько это обойдется?'),
-expenses2 = prompt('Введите обязательную статью расходов?'),
-amount2 = +prompt('Во сколько это обойдется?'),
-budgetMonth = money - (amount1 + amount2),
-budgetDay = Math.floor(budgetMonth / 30);
+   income = 'freelance', 
+   addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую').toLowerCase().split(', '),
+   deposit = confirm('Есть ли у вас депозит в банке?'), 
+   mission = 20000, 
+   period = 12, 
+   expenses1 = prompt('Введите обязательную статью расходов?'),
+   amount1 = +prompt('Во сколько это обойдется?'),
+   expenses2 = prompt('Введите обязательную статью расходов?'),
+   amount2 = +prompt('Во сколько это обойдется?'),
+   budgetMonth = money - (amount1 + amount2),
+   budgetDay = Math.floor(budgetMonth / 30);
 
 console.log( typeof(money), typeof(income), typeof(deposit) );
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' долларов');
-console.log(addExpenses);
 console.log(budgetDay, addExpenses, budgetMonth);
 
 if (budgetMonth > 0) {
