4948e532ca3ede786c79b6abd288227ca59b100f
Anatoly Kebets <akebecz@imbox.ru>
2021-04-07 19:18:53 +0300

    Done

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 'use strict';
-function fun(arg) {
-   if ( typeof(arg) !== 'string') {
-      console.log('Вы передали не строку');
-   } else {
-      arg = arg.trim();
-      if (arg.length > 30) {
-         console.log( arg.slice(0, 29) + '...');        
-      } else {
-         console.log(arg);
-      }  
+const arr = ['123', '242', '533', '431', '7654', '2223', '42'];
+for (let i = 0; i < 7; i++) {
+   for (let y = 0; y < arr[i].length; y++) {
+      if ( arr[i][0] === '2' || arr[i][0] === '4') {
+         console.log( arr[i] );
+      }
    }
 }
-const str = '   Hi my name is Anatoly! I live in Belarus, Belarus is the centre of Europe.   ';
-fun(str);
+// 2 задание
+function cheking(n) {
+   let k = 0;
+   for (let i = 0; i <= n; i++) {
+      if (k > 2) {
+         break;
+      }
+      else if (n % i === 0) {
+         k++;
+      }
+   }
+   if (k === 2) {
+      console.log( n + ' - Простое число  И его делители - 1 и ' + n );
+   }
+}
+for (let y = 2; y < 101; y++) {
+   cheking(y);
+}
