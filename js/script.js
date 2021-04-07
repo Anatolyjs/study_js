646dfd5d70e1147f8ba54fcc4c0d88b443076be1
Anatoly Kebets <akebecz@imbox.ru>
2021-04-07 22:22:58 +0300

    Changed

--- js/script.js
+++ js/script.js
@@ -9,12 +9,13 @@ const income = 'freelance',
    sum = [],
 
    isNumber = function(n) {
-      return !isNaN( parseFloat(n) ) && isFinite(n);
+      return !isNaN( parseFloat(n) ) && isFinite(n) && n !== 0;
    },
 
    start = function() {
       do {
-      money = prompt('Ваш месячный доход?');
+         money = +prompt('Ваш месячный доход?');
+         console.log( money);
       } while( !isNumber(money) ); 
    },
 
@@ -27,39 +28,38 @@ const income = 'freelance',
       for (let i = 0; i < 2; i++) {
          exspenses[i] = prompt('Введите обязательную статью расходов?');
          do {
-            sum[i] = prompt('Во сколько это обойдется?');
+            sum[i] = +prompt('Во сколько это обойдется?');
          } while( !isNumber(sum[i]) ); 
          temp += +sum[i];
       }
-      console.log(temp);
       return temp;
-   },
-   ExpensesAmount = getExpensesMonth();
-   start();
+   };
+start();
+const ExpensesAmount = getExpensesMonth(),
 
-   const getAccomulateMonth = function () {
-      return money - ExpensesAmount;
-   },
-   accumulateMonth = getAccomulateMonth(),
+      getAccomulateMonth = function () {
+         return money - ExpensesAmount;
+      },
+      accumulateMonth = getAccomulateMonth(),
 
-   getTargetMonth = function (mission, accumulateMonth) {
-      return Math.ceil(mission / accumulateMonth);
-   },
+      getTargetMonth = function (mission, accumulateMonth) {
+         return Math.ceil(mission / accumulateMonth);
+      },
 
-   budgetDay = Math.floor(accumulateMonth / 30),
-   getStatusIncome = function() {
-      if (budgetDay  >= 1200) {
-         console.log('У вас высокий уровень дохода');
-      } else if (budgetDay >= 600 && budgetDay < 1200) {
-         console.log('У вас средний уровень дохода');
-      } else if (budgetDay > 0 && budgetDay < 600) {
-         console.log('К сожалению, у вас уровень дохода ниже среднего');
-      } else if (budgetDay === 0) {
-         console.log('Ваш дневной бюджет равен 0! Нужно что-то с этим делать!');
-      } else {
-         console.log('Что-то пошло не так!');
-      }
- };
+      budgetDay = Math.floor(accumulateMonth / 30),
+      getStatusIncome = function() {
+         if (budgetDay  >= 1200) {
+            console.log('У вас высокий уровень дохода');
+         } else if (budgetDay >= 600 && budgetDay < 1200) {
+            console.log('У вас средний уровень дохода');
+         } else if (budgetDay > 0 && budgetDay < 600) {
+            console.log('К сожалению, у вас уровень дохода ниже среднего');
+         } else if (budgetDay === 0) {
+            console.log('Ваш дневной бюджет равен 0! Нужно что-то с этим делать!');
+         } else {
+            console.log('Что-то пошло не так!');
+         }
+   };
 
 showTypeOf(money);
 showTypeOf(income);
