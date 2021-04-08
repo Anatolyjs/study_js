cb5329f0096bee5ef44fdcdeddfda263c69262d7
Anatoly Kebets <akebecz@imbox.ru>
2021-04-09 00:42:15 +0300

    Done

--- js/script.js
+++ js/script.js
@@ -3,32 +3,48 @@ let number;
 const isNumber = function(n) {
    return !isNaN( parseFloat(n) ) && isFinite(n) && n !== 0;
 },
-
-   start = function() {  
-      number = prompt('Угадай число от 0 до 100');
-      const x = 45,
-      checking = function () {
-         if ( isNumber(number) ) {
-            if (number > x) {
-               alert('Заданное число меньше');
-               start();
-            } else if (number < x) {
-               alert('Заданное число больше');
-               start();
-            } else if (number === x) {
-               alert('Поздравляю! Вы угадали!');
-            } 
-         } else {
-            alert('Введи число!');
+glFun = function () {
+   let counter = 10,
+      x = Math.floor(Math.random() * 100 + 1);
+   const start = function() {  
+      if (counter === 0) {
+         const questioning = confirm('Ваши попытки закончились! Хотите сыграть еще раз?');
+         if (questioning) {
+            counter = 10;
+            Math.floor(Math.random() * 100 + 1);
             start();
+         } else {
+            alert('До новых встреч!');
          }
-      };
-      
-      if (number === null) {
-         alert('Игра окончена.');
       } else {
-         number = +number;
-         checking();
+         number = prompt('Угадай число от 0 до 100');
+         const checking = function () {
+            if ( isNumber(number) ) {
+               counter--;
+               if (number > x) {
+                  alert('Заданное число меньше');
+                  start();
+               } else if (number < x) {
+                  alert('Заданное число больше');
+                  start();
+               } else if (number === x) {
+                  alert('Поздравляю! Вы угадали!');
+               } 
+            } else {
+               alert('Введи число!');
+               start();
+            }
+         };
+         
+         if (number === null) {
+            alert('Игра окончена.');
+         } else {
+            number = +number;
+            checking();
+         }
       }
    };
-start();
+   return start;
+},
+gettting = glFun();
+gettting();
