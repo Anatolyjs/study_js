7ace259dabf73082332830a81b15ce17cf6613fb
Anatoly Kebets <akebecz@imbox.ru>
2021-04-08 23:00:07 +0300

    Changed

--- js/script.js
+++ js/script.js
@@ -1,30 +1,38 @@
 'use strict';
 let number;
-const x = 45,
-   isNumber = function(n) {
-      return !isNaN( parseFloat(n) ) && isFinite(n) && n !== 0;
-   },
-
-   start = function() {  
-      number = prompt('Угадай число от 0 до 100');
-      if (number === null) {
-         alert('Игра окончена.');
-      } else {
-         number = +number;
-         if ( isNumber(number) ) {
-            if (number > x) {
-               alert('Заданное число меньше');
+const isNumber = function(n) {
+   return !isNaN( parseFloat(n) ) && isFinite(n) && n !== 0;
+},
+glFun = function () {
+   const x = Math.floor(Math.random() * 100 + 1),
+      start = function() {  
+         number = prompt('Угадай число от 0 до 100');
+         const checking = function () {
+            if ( isNumber(number) ) {
+               if (number > x) {
+                  alert('Заданное число меньше');
+                  start();
+               } else if (number < x) {
+                  alert('Заданное число больше');
+                  start();
+               } else if (number === x) {
+                  alert('Поздравляю! Вы угадали!');
+               } 
+            } else {
+               alert('Введи число!');
                start();
-            } else if (number < x) {
-               alert('Заданное число больше');
-               start();
-            } else if (number === x) {
-               alert('Поздравляю! Вы угадали!');
-            } 
+            }
+         };
+      
+         if (number === null) {
+            alert('Игра окончена.');
          } else {
-            alert('Введи число!');
-            start();
+            number = +number;
+            checking();
          }
-      }
-   };
-start();
+      };
+   return start;
+},
+gettting = glFun();
+gettting();
+
