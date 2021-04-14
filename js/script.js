1e0767124ae578cfd1eacf84f56ca18a198fd75e
Anatoly Kebets <Akebecz@inbox.ru>
2021-04-14 17:17:52 +0300

    Done

---
+++ js/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const books = document.querySelector('.books'),
+    book = document.querySelectorAll('.book'),
+    advertising = document.querySelector('.adv'),
+    chapterSecondBook = book[0].querySelectorAll('li'),
+    chapterFifthBook = book[5].querySelectorAll('li'),
+    chapterSixsBook = book[2].querySelectorAll('li'),
+    newChapter = document.createElement('li'),
+    book4Style = book[4].querySelector('h2').innerHTML = '<a target=_blank href=https://github.com/azat-io/you-dont-know-js-ru/blob/master/this%20%26%20object%20prototypes/README.md#you-dont-know-js-this--object-prototypes>Книга 3. this и Прототипы Объектов</a>';
+books.prepend( book[1] );
+books.append( book[2] );
+book[0].after( book[4] );
+document.body.style.backgroundImage = 'url(./image/adv.jpg)';
+advertising.remove();
+console.log(book);
+// вторая книга
+chapterSecondBook[3].after( chapterSecondBook[6] );
+chapterSecondBook[6].after( chapterSecondBook[8] );
+chapterSecondBook[9].after( chapterSecondBook[2] );
+// пятая книга
+chapterFifthBook[1].after( chapterFifthBook[9] );
+chapterFifthBook[4].after( chapterFifthBook[2] );
+chapterFifthBook[7].after( chapterFifthBook[5] );
+// шестая книга
+newChapter.textContent = 'Глава 8: За пределами ES6';
+chapterSixsBook[8].insertAdjacentElement('afterend', newChapter);
+console.log(chapterSixsBook[8]);
+
+
