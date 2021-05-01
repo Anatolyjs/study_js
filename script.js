418cc2385d96296cc52c84d9527f6d1736c48489
Anatoly Kebets <Akebecz@inbox.ru>
2021-05-01 22:52:05 +0300

    Done

--- script.js
+++ script.js
@@ -313,6 +313,57 @@ window.addEventListener('DOMContentLoaded', function() {
                 }
             });
         });
+    },
+
+    //Калькулятор
+    calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total'),
+            calcBlockInputs = document.querySelectorAll('.calc-block input');
+        
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            } else {
+                total = 0;
+            }
+            
+            totalValue.textContent = total;
+        };
+        
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+            
+            if (target.matches('select') || target.matches('input')) {
+                countSum();
+            }
+        });
+
+        calcBlockInputs.forEach((item) => {
+            item.addEventListener('input', () => {
+                item.value = item.value.replace(/^[\D]/g, '');
+            });
+        });
     };
 
     countTimer('25 april 2021');
@@ -322,6 +373,7 @@ window.addEventListener('DOMContentLoaded', function() {
     tabs();
     slider();
     regCheking();
+    calc(100);
 
     
     
