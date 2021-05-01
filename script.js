2eb88778a8d2cac104dfe50e5c68884a75de13b8
Anatoly Kebets <Akebecz@inbox.ru>
2021-05-01 21:03:56 +0300

    Done

--- script.js
+++ script.js
@@ -260,8 +260,60 @@ window.addEventListener('DOMContentLoaded', function() {
         });
 
         startSlide(1000);
-    };
+    },
 
+    //Регулярки
+    regCheking = () => { 
+        const commandImg = document.querySelectorAll('.command .container .row .col-md-4 img'),
+            inputs = document.querySelectorAll('input');
+        
+        const checkInputsBlur = (target) => {
+            if (target.matches('.calc-block input')) {
+                target.value = target.value.replace(/\D/g, '');
+            } else if (target.matches('[placeholder="Ваше имя"]') || target.matches('[placeholder="Ваше сообщение"]')) {
+                target.value = target.value.replace(/[^а-я- ]/gi, '');
+            } else if (target.matches('[placeholder="E-mail"]') || target.matches('[placeholder="Ваш E-mail"]')) {
+                target.value = target.value.replace(/[^a-z-@_.!~*']/gi, '');
+            } else if (target.matches('[placeholder="Номер телефона"]') || target.matches('[placeholder="Ваш номер телефона"]')) {
+                target.value = target.value.replace(/[^\d()-]/gi, '');
+            }
+        };
+
+        commandImg.forEach((item) => {
+            item.addEventListener('mouseenter', (event) => {
+                event.target.src = event.target.dataset.img;
+            });
+
+            item.addEventListener('mouseleave', (event) => {
+                event.target.src = event.target.src.replace(/a(?=.jpg)/, '');
+            });
+        });
+
+        inputs.forEach((item) => {
+            item.addEventListener('input', (event) => {
+                const target = event.target;
+                checkInputsBlur(target);
+            });
+
+            item.addEventListener('blur', (event) => {
+                const target = event.target;
+
+                checkInputsBlur(target);
+
+                target.value = target.value.trim();
+                target.value = target.value.replace(/^-+/g, '');
+                target.value = target.value.replace(/-+$/g, '');
+                target.value = target.value.replace(/-+/g, '-');
+                target.value = target.value.replace(/ +/g, ' ');
+
+                if (target.matches('[placeholder="Ваше имя"]')) {
+                    target.value = target.value.replace(/( |^)[а-яa-z]+/gi, (val1) => val1.toLowerCase());
+                    target.value = target.value.replace(/( |^)[а-яa-z]/gi, (val2) =>  val2.toUpperCase());
+                   
+                }
+            });
+        });
+    };
 
     countTimer('25 april 2021');
     togleMenu();
@@ -269,15 +321,9 @@ window.addEventListener('DOMContentLoaded', function() {
     scrollsMenu();
     tabs();
     slider();
+    regCheking();
 
-    const command = document.querySelectorAll('.command .container .row .col-md-4 img');
-    command.forEach((item) => {
-        item.addEventListener('mouseenter', (event) => {
-            event.target.src = event.target.dataset.img;
-            console.log(event.target.src);
-        });
-    });
-
+    
     
  
 });
