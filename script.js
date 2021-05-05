8ab08cd9dada28fe375695beb0e989ddaa7287e0
Anatoly Kebets <Akebecz@inbox.ru>
2021-05-06 01:51:25 +0300

    doing

--- script.js
+++ script.js
@@ -375,8 +375,35 @@ window.addEventListener('DOMContentLoaded', function() {
         const form = document.getElementById('form1');
 
         const statusMessage = document.createElement('div');
-        statusMessage.textContent = 'Тут будет наше сообщение';
-        form1.append(statusMessage);
+        statusMessage.style.cssText = 'font-size: 2rem';
+        
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            form.appendChild(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.addEventListener('readystatechange', () => {
+                statusMessage.textContent = loadMessage;
+                if (request.readyState !== 4) {
+                    return;
+                }
+                if (request.status === 200) {
+                    statusMessage.textContent = successMessage;
+                } else {
+                    statusMessage.textContent = errorMessage;
+                }
+            });
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-Type', 'application/json');
+            const formData = new FormData(form);
+            let body = {};
+            
+            for (let val of formData.entries()) {
+                body[val[0]] = val[1];
+            }
+            request.send(JSON.stringify(body));
+            
+        });
         
 
     };
